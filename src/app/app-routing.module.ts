57bc7f93781f04fa8801f58a91c8cc924b9e6dd9
Aviv Ben Shahar <avivbens87@gmail.com>
2021-08-18 14:23:54 +0300

    hash router fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ export class FeatureRoutingModule { }
 
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
